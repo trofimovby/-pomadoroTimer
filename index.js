281b757dae92d85bb3f95f49726b5bdc67c25d13
Trofimov Alexander <trofas03@yandex.ru>
2022-08-21 20:41:14 +0300

    Add Switch Mode

--- index.js
+++ index.js
@@ -4,3 +4,30 @@ const timer = {
     longBreak: 15,
     longBreakInterval: 4
 };
+
+const modeButtons = document.querySelector('#js-mode-buttons');
+
+modeButtons.addEventListener('click', handleMode);
+
+function handleMode() {
+    const {mode} = event.target.dataset;
+    if(!mode) return;
+
+    switchMode(mode);
+}
+
+function switchMode(mode) {
+    timer.mode = mode;
+    timer.remainingTime = {
+        total: timer[mode] * 60,
+        minutes: timer[mode],
+        seconds: 0,
+    };
+
+    document
+        .querySelectorAll('button[data-mode]')
+        .forEach(e => e.classList.remove('active'));
+    document.querySelector(`[data-mode="${mode}"]`).classList.add('active');
+    document.body.style.backgroundColor = `var(--${mode})`;
+    
+}
